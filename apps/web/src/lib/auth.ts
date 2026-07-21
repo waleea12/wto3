460b147355a1b63627fe45e040349d11626b241c
WLEEDCOED <nnqq125@gmail.com>
2026-07-21 14:09:48 +0300

    Increase session maxAge to 30 days to remember users longer

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -103,7 +103,7 @@ export const authOptions: NextAuthOptions = {
   },
   session: {
     strategy: 'jwt',
-    maxAge: 7 * 24 * 60 * 60,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   secret: process.env.NEXTAUTH_SECRET,
 }
